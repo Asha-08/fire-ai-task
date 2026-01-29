75b4645e26bd44b00d58cfae1fd09d3ece93b335
Asha-08 <ashaamina91@gmail.com>
2026-01-29 18:47:17 +0600

    small change on rightside component

--- src/components/DasboardComponent/RightSide.jsx
+++ src/components/DasboardComponent/RightSide.jsx
@@ -9,21 +9,21 @@ const RightSide = () => {
             <p className='text-[#C0CCDD]'>156 requests</p>
         </div>
         <div className='w-full h-3 rounded-2xl bg-[#1D293D]'>
-            <div className='w-75 h-full bg-linear-to-b from-[#2B7FFF] to-[#00B8DB] rounded-2xl'></div>
+            <div className='w-55 h-full bg-linear-to-b from-[#2B7FFF] to-[#00B8DB] rounded-2xl'></div>
         </div>
         <div className='flex justify-between p-4'>
             <p className='text-[#FFFFFF]'>Battery Replacement</p>
             <p className='text-[#C0CCDD]'>89 requests</p>
         </div>
         <div className='w-full h-3 rounded-2xl bg-[#1D293D]'>
-            <div className='w-55 h-full bg-linear-to-b from-[#2B7FFF] to-[#00B8DB] rounded-2xl'></div>
+            <div className='w-45 h-full bg-linear-to-b from-[#2B7FFF] to-[#00B8DB] rounded-2xl'></div>
         </div>
         <div className='flex justify-between p-4'>
             <p className='text-[#FFFFFF]'>Back Glass Repair</p>
             <p className='text-[#C0CCDD]'>67 requests</p>
         </div>
         <div className='w-full h-3 rounded-2xl bg-[#1D293D]'>
-            <div className='w-45 h-full bg-linear-to-b from-[#2B7FFF] to-[#00B8DB] rounded-2xl'></div>
+            <div className='w-40 h-full bg-linear-to-b from-[#2B7FFF] to-[#00B8DB] rounded-2xl'></div>
         </div>
         <div className='flex justify-between p-4'>
             <p className='text-[#FFFFFF]'>Software Issues</p>
